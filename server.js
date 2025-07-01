63da23af1e2c1f39f31b2ff1b3c45288cda2fc6d
Your Name <you@example.com>
2025-07-01 10:35:24 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js';
+
+const server = fastify();
+
+const database = new DatabaseMemory()
+
+server.post("/videos", (request, reply) => {
+const {title, description, duration } = request.body
+
+
+
+    database.create({
+        title,
+        description,
+        duration
+    })
+
+    return reply.status(201).send()
+})
+
+server.get("/videos", () => {
+    const videos = database.list()
+
+    return videos
+})
+
+server.put("/videos/:id", () => {
+    return 'olá moundo'
+})
+
+server.delete("/videos/:id", () => {
+    return 'olá moundo'
+})
+
+server.listen({
+    port: 3333,
+})
+
+ 
